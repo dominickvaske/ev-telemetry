2a0dec4fda9ae64f6bb5ba623edce52f79b763e1
Dominick Vaske <dominickvaske@gmail.com>
2026-04-09 21:10:21 -0400

    Created FleetStore struct and associated functions

--- main.go
+++ main.go
@@ -5,6 +5,36 @@ import (
 	"time"
 )
 
+type FleetStore struct {
+	vehicles map[string]Vehicle
+}
+
+func newFleetStore() *FleetStore {
+	return &FleetStore{
+		vehicles: make(map[string]Vehicle),
+	}
+}
+
+// Add a vehicle to the fleetstore
+func (fs *FleetStore) Add(v Vehicle) {
+	fs.vehicles[v.ID] = v
+}
+
+// Get a vehicle from a string
+func (fs *FleetStore) Get(id string) (Vehicle, bool) {
+	val, ok := fs.vehicles[id]
+	return val, ok
+}
+
+// List all vehicles currently in fleetstore
+func (fs *FleetStore) List() []Vehicle {
+	var result []Vehicle
+	for _, vehicle := range fs.vehicles {
+		result = append(result, vehicle)
+	}
+	return result
+}
+
 type Vehicle struct {
 	ID         string
 	BatteryPct float64
@@ -19,9 +49,13 @@ func main() {
 	v2 := Vehicle{ID: "V-002", BatteryPct: 70.0, SpeedKPH: 66.0, TempC: 24.0, IsCharging: false, Timestamp: time.Now()}
 	v3 := Vehicle{ID: "V-003", BatteryPct: 43.0, SpeedKPH: 32.0, TempC: 22.0, IsCharging: false, Timestamp: time.Now()}
 
-	vehicles := []Vehicle{v1, v2, v3}
+	store := newFleetStore()
 
-	for _, vehicle := range vehicles {
+	store.Add(v1)
+	store.Add(v2)
+	store.Add(v3)
+
+	for _, vehicle := range store.List() {
 		fmt.Println(vehicle)
 	}
 }
