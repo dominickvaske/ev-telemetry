c70d76c3d75a28e1e3e1eb97109190b7a6a21feb
Dominick Vaske <dominickvaske@gmail.com>
2026-05-12 20:50:25 -0400

    SimulateVehicle function written to update battery for single vehicle and send over channel

--- internal/sim/sim.go
+++ internal/sim/sim.go
@@ -5,8 +5,29 @@ import (
 	"time"
 
 	"github.com/dominickvaske/ev-telemetry/internal/alert"
+	"github.com/dominickvaske/ev-telemetry/internal/fleet"
 	"github.com/dominickvaske/ev-telemetry/internal/store"
 )
+
+// SimulateVehicle runs simulation for a single vehicle v
+// v: fleet.Vehicle being simulated on specific goroutine
+// telemetryCh: channel shared by ingest function to know when to update
+// done: channel shared with main to receive done signal
+func SimulateVehicle(v fleet.Vehicle, telemetryCh chan<- fleet.Vehicle, done chan struct{}) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			v.BatteryPct--
+			v.Timestamp = time.Now()
+			telemetryCh <- v
+		case <-done:
+			return
+		}
+	}
+}
 
 // SimulateTick runs through all vehicles and drops battery
 // percentage by one point to simulate a drive time instance
